21fe761eb13729e36edd937d8f727d9002cb8d78
Melanie <melanie.tonarelli@mail.polimi.it>
2024-12-07 14:12:42 +0100

    modified test

--- fake-paxos/test.py
+++ fake-paxos/test.py
@@ -19,8 +19,6 @@ if __name__ == "__main__":
     config["acceptors"] = ("239.0.0.1", 7000)
     config["learners"] = ("239.0.0.1", 8000)
     
-    #proposer2 = Proposer(2, config)
-    #threading.Thread(target = proposer2.run).start()
     
     client1 = Client(1, config)
     threading.Thread(target = client1.run_file, args=[f"input_tests/test1_{x}"]).start()
@@ -47,13 +45,12 @@ if __name__ == "__main__":
     threading.Thread(target = acceptor3.run).start()
 
     
-
     time.sleep(10)
     client3 = Client(3, config)
     threading.Thread(target = client3.run_file, args=[f"input_tests/test1_{x}"]).start()
 
-    # time.sleep(10)
-    # learner2 = Learner(2, config)
-    # threading.Thread(target = learner2.run_file, args=["output2.txt"]).start()
+    time.sleep(10)
+    learner2 = Learner(2, config)
+    threading.Thread(target = learner2.run_file, args=["output2.txt"]).start()
 
     time.sleep(60)
